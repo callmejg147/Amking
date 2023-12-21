879e308d94f75f4123b62d91f905819bb4e8d1a5
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-21 10:46:30 +0530

    Update settings.py

--- amking_web/settings.py
+++ amking_web/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Yeahthisisamking'#os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get('D_MODE', 'False').lower() == 'true'
+DEBUG = os.environ.get('D_MODE', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
@@ -97,8 +97,8 @@ DATABASES = {
     }
 }
 
-#db_url = os.environ.get('DB_URL')
-#DATABASES['default'] = dj_database_url.parse(db_url)
+db_url = os.environ.get('DB_URL')
+DATABASES['default'] = dj_database_url.parse(db_url)
 
 
 # Password validation
@@ -135,9 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = "/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
