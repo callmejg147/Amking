668c59b7554a18eb0f55784c7a9cc430847c5274
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-21 16:22:07 +0530

    Update urls.py

--- amking_web/urls.py
+++ amking_web/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
     path('services', services_view, name='services'),
     path('testimonials/', testimonial_view , name='testimonial'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
