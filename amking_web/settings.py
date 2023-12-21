6c6c54f30e5b197fdd315dc9590b7519372e917c
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-21 18:20:23 +0530

    Update settings.py

--- amking_web/settings.py
+++ amking_web/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
 
-if settings.DEBUG:
+if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
