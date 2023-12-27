d484177c94b6704739332b824ed92ba4ff7d77bb
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-27 06:38:11 +0000

    updated urlpatterns

--- amking_web/urls.py
+++ amking_web/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
     path('testimonials/', testimonial_view , name='testimonial'),
     path('admin/', admin.site.urls),
 ] 
-#urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += staticfiles_urlpatterns()
