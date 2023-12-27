84021bdad12fd667d936683a30953e7fe38f161f
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-27 09:27:45 +0000

    changed back to meedia root

--- amking_web/settings.py
+++ amking_web/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
-DJANGO_MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
