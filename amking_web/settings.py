87cee8b9c04dc47bc6a9a03d8c62b72a26ce84ee
Jithin George <104771188+callmejg147@users.noreply.github.com>
2023-12-27 06:05:07 +0000

    updates

--- amking_web/settings.py
+++ amking_web/settings.py
@@ -143,6 +143,7 @@ if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
