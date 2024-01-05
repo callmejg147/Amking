69f9c03716fa59c73fadfdf598fd5e1da29f93f7
Jithin George <104771188+callmejg147@users.noreply.github.com>
2024-01-05 11:38:37 +0530

    Update settings.py

--- amking_web/settings.py
+++ amking_web/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('D_MODE', 'True').lower() == 'True'
+DEBUG = True 
+#os.environ.get('D_MODE', 'True').lower() == 'True'
 
 ALLOWED_HOSTS = ['*']
 
