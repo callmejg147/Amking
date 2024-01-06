63efc4fc18518d87375537552ff63e5d079139e9
Jithin George <104771188+callmejg147@users.noreply.github.com>
2024-01-06 12:43:52 +0530

    Update admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Category, Gallery
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Gallery)
+class Categoryadmin(admin.ModelAdmin):
+    list_display=['name']
+admin.site.register(Category, Categoryadmin)
+
+class Galleryadmin(admin.ModelAdmin):
+    list_display=['title', 'category', 'image']
+admin.site.register(Gallery, Galleryadmin)
