96916d12ae2586d2a949f2645381d706cd030701
Jithin George <104771188+callmejg147@users.noreply.github.com>
2024-01-06 14:38:53 +0530

    Update admin.py

--- amking_projects/admin.py
+++ amking_projects/admin.py
@@ -3,5 +3,5 @@ from .models import Projects
 
 # Register your models here.
 class Projectadmin(admin.ModelAdmin):
-    list_display=['title', 'short_description', 'description', 'img1', 'img2', 'img3', 'img4']
+    list_display=['title', 'short_description', 'description', 'img1', 'img2', 'img3', 'img4', 'location']
 admin.site.register(Projects, Projectadmin)
