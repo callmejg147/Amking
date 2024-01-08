88780db193a625afe4c7b3157879bdefa5f7e2e1
Jithin George <104771188+callmejg147@users.noreply.github.com>
2024-01-08 14:54:34 +0530

    Update views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from .models import Category, Gallery
+from .models import Category, Gallery, GalleryVid
 # Create your views here.
 
 
 def gallery_view(request, *args, **kwargs):
     cat = Category.objects.all()
     gal = Gallery.objects.all().order_by('-id')
+    vid = GalleryVid.objects.all().order_by('-id')
     context = {
         'category': cat,
         'images': gal,
+        'videos':vid,
     }
     return render(request, 'gallery.html', context=context)
