8416c75ff1d2817ad5c5d7801de461e678115893
Jithin George <104771188+callmejg147@users.noreply.github.com>
2024-01-08 14:55:20 +0530

    Update models.py
    added videos model

--- gallery/models.py
+++ gallery/models.py
@@ -19,3 +19,11 @@ class Gallery(models.Model):
     objects = models.Manager()
     class Meta:
         verbose_name_plural = 'Gallery'
+
+class GalleryVid(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False)
+    video = models.FileField(upload_to='videos/', null=True)
+    objects = models.Manager()
+    class Meta:
+        verbose_name_plural = 'Videos'
